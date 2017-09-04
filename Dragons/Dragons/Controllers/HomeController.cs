d914e89602f20e16c95ded1f63e601c7b598f82a
Jimmy Alfaro <jimalfa182@gmail.com>
2017-09-04 16:27:51 -0600

    change

--- Dragons/Dragons/Controllers/HomeController.cs
+++ Dragons/Dragons/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace Dragons.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+
         }
 
         public ActionResult Contact()
